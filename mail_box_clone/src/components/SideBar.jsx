5473dc7c546b76cc842693c56b8dca62518dfd75
CoolShivas <shivas2710cool00@gmail.com>
2024-06-13 16:09:52 +0530

    Added the Rest of Button Icons with text and render it using map method.

--- mail_box_clone/src/components/SideBar.jsx
+++ mail_box_clone/src/components/SideBar.jsx
@@ -6,6 +6,37 @@ import { MdOutlineWatchLater } from "react-icons/md";
 import { IoIosStar } from "react-icons/io";
 import { LuPencil } from "react-icons/lu";
 
+
+
+const sideBarItems = [
+    {
+        icons: <RiInbox2Fill size={"24px"} />,
+        text: "Inbox"
+    },
+    {
+        icons: <MdOutlineWatchLater size={"24px"} />,
+        text: "Starred"
+    },
+    {
+        icons: <TbSend2 size={"24px"} />,
+        text: "Snoozed"
+    },
+    {
+        icons: <IoIosStar size={"24px"} />,
+        text: "Sent"
+    },
+    {
+        icons: <MdOutlineDrafts size={"24px"} />,
+        text: "Drafts"
+    },
+    {
+        icons: <MdKeyboardArrowDown size={"24px"} />,
+        text: "More"
+    },
+];
+
+
+
 const SideBar = () => {
     return (
         <div className="w-[15%]">
@@ -18,9 +49,10 @@ const SideBar = () => {
             </div>
             {/* Ending of Compose button with Icon */}
 
-            {/* Starting of Hard Coded Rest of the Button Icons with text */}
+            {/* Starting of Rest of the Button Icons with text */}
             <div className="text-gray-500">
-                <div className="flex items-center gap-4 pl-6 py-1 rounded-r-full hover:cursor-pointer hover:bg-gray-200 my-2">
+
+                {/* <div className="flex items-center gap-4 pl-6 py-1 rounded-r-full hover:cursor-pointer hover:bg-gray-200 my-2">
                     <RiInbox2Fill size={"24px"} />
                     <p> Inbox </p>
                 </div>
@@ -43,10 +75,22 @@ const SideBar = () => {
                 <div className="flex items-center gap-4 pl-6 py-1 rounded-r-full hover:cursor-pointer hover:bg-gray-200 my-2">
                     <MdKeyboardArrowDown size={"24px"} />
                     <p> More </p>
-                </div>
+                </div> */}
+
+
+                {/* Starting of using map and rendering Button Icons with text  */}
+
+                {sideBarItems.map((arr, index) => {
+                    return <div className="flex items-center gap-4 pl-6 py-1 rounded-r-full hover:cursor-pointer hover:bg-gray-200 my-2" key={index}>
+                        {arr.icons} {arr.text}
+                    </div>
+                })}
+
+                {/* Ending of using map and rendering Button Icons with text  */}
+
 
             </div>
-            {/* Ending of Hard Coded Rest of the Button Icons with text */}
+            {/* Ending of Rest of the Button Icons with text */}
 
         </div>
     )
