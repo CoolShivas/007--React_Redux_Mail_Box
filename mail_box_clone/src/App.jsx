de56f6f4fabb4af415667f33f94e7850875f38dc
CoolShivas <shivas2710cool00@gmail.com>
2024-06-13 09:24:58 +0530

    Installation of Tailwind CSS and configure it.

--- mail_box_clone/src/App.jsx
+++ mail_box_clone/src/App.jsx
@@ -1,8 +1,8 @@
 const App = () => {
   return (
     <>
-      <h1> Mail Box Client i.e, </h1>
-      <h1> Clone of Gmail using Tailwind CSS </h1>
+      <h1 className="text-red-500"> Mail Box Client i.e, </h1>
+      <h1 className="text-blue-500"> Clone of Gmail using Tailwind CSS </h1>
     </>
   )
 }
