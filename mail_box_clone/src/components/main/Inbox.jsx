660bc1421ddd71992efc2ff72ef732106d3e2e3c
CoolShivas <shivas2710cool00@gmail.com>
2024-06-15 12:54:22 +0530

    Second Row of Body Mid Section and render the MailTypes  using MapMethod

--- mail_box_clone/src/components/main/Inbox.jsx
+++ mail_box_clone/src/components/main/Inbox.jsx
@@ -1,13 +1,32 @@
-import { MdCropSquare } from "react-icons/md";
+import { GoTag } from "react-icons/go";
+import { IoMdMore } from "react-icons/io";
+import { MdCropSquare, MdInbox } from "react-icons/md";
 import { IoMdRefresh } from "react-icons/io";
-import { FaCaretDown } from "react-icons/fa";
+import { FaCaretDown, FaUserFriends } from "react-icons/fa";
+
+
+const mailTypes = [
+    {
+        icon: <MdInbox size={"20px"} />,
+        text: "Primary"
+    },
+    {
+        icon: <GoTag size={"20px"} />,
+        text: "Promotion"
+    },
+    {
+        icon: <FaUserFriends size={"20px"} />,
+        text: "Social"
+    },
+];
+
 
 const Inbox = () => {
     return (
         <div className="flex-1 bg-red-100 rounded-xl mx-5">
             <div className="flex items-center bg-orange-200 justify-between px-4">
 
-                {/* Starting of BODY MID SECTION Icons  */}
+                {/* Starting of BODY Mid Section Right Side Icons (First-Row) */}
                 <div className="flex items-center gap-2 text-gray-700 py-2">
                     <div className="flex items-center gap-1">
                         <MdCropSquare size={"20px"} />
@@ -16,10 +35,27 @@ const Inbox = () => {
                     <div className="p-2 rounded-full hover:bg-gray-100 cursor-pointer">
                         <IoMdRefresh size={"20px"} />
                     </div>
+                    <div className="p-2 rounded-full hover:bg-gray-100 cursor-pointer">
+                        <IoMdMore size={"20px"} />
+                    </div>
                 </div>
-                {/* Ending of BODY MID SECTION Icons  */}
+                {/* Ending of BODY Mid Section Right Side Icons (First-Row) */}
 
             </div>
+
+            {/* Starting of BODY Mid Section Right Side Icons (Seconde-Row) */}
+            <div className="h-[10vh] overflow-y-auto bg-blue-200">
+                <div className="flex items-center gap-1">
+                    {mailTypes.map((brr, index) => {
+                        return <button key={index}>
+                            {brr.icon}
+                            <span> {brr.text} </span>
+                        </button>
+                    })}
+                </div>
+            </div>
+            {/* Ending of BODY Mid Section Right Side Icons (Second-Row) */}
+
         </div>
     )
 }
