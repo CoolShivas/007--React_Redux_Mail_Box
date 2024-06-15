774257a475983852c5ec8021b546a82567bb4117
CoolShivas <shivas2710cool00@gmail.com>
2024-06-15 13:15:14 +0530

    Added CSS in MailTypes button with hover

--- mail_box_clone/src/components/main/Inbox.jsx
+++ mail_box_clone/src/components/main/Inbox.jsx
@@ -23,8 +23,8 @@ const mailTypes = [
 
 const Inbox = () => {
     return (
-        <div className="flex-1 bg-red-100 rounded-xl mx-5">
-            <div className="flex items-center bg-orange-200 justify-between px-4">
+        <div className="flex-1 rounded-xl mx-5 bg-white">
+            <div className="flex items-center justify-between px-4">
 
                 {/* Starting of BODY Mid Section Right Side Icons (First-Row) */}
                 <div className="flex items-center gap-2 text-gray-700 py-2">
@@ -44,10 +44,12 @@ const Inbox = () => {
             </div>
 
             {/* Starting of BODY Mid Section Right Side Icons (Seconde-Row) */}
-            <div className="h-[10vh] overflow-y-auto bg-blue-200">
+            <div className="h-[10vh] overflow-y-auto">
                 <div className="flex items-center gap-1">
                     {mailTypes.map((brr, index) => {
-                        return <button key={index}>
+                        return <button
+                            className="flex items-center gap-5 p-4 w-52 hover:bg-gray-100"
+                            key={index}>
                             {brr.icon}
                             <span> {brr.text} </span>
                         </button>
