c69e9e3906640455c2db7d883e40e5d034d78c62
CoolShivas <shivas2710cool00@gmail.com>
2024-06-20 16:22:02 +0530

    Added onCLick event listener using dispatch action on SideBar Page to open the Compose Box i.e, SendMail. As well as using useSelector to select the initial state and render it by condition whether block or hidden when the action is dispatch. Therefore, Compose Box is open.

--- mail_box_clone/src/components/pages/SendMail.jsx
+++ mail_box_clone/src/components/pages/SendMail.jsx
@@ -1,8 +1,20 @@
 import { RxCross2 } from "react-icons/rx";
+import { useSelector } from "react-redux";
 
 const SendMail = () => {
+
+    // const [openCompose, setOpenCompose] = useState(false);
+
+    // So, we in the SideBar and the Compose Btn is located here. But, the most important thing is that Compose Functional Component i.e, SendMail is an another file and to implement this local useState Hook either we have to make the SendMail Function here. Or we can pass the openCompose as props to that place. 
+
+    // What is local state variable ? 
+
+    //   This is a local state variable that can be used in this functional component or either can be accessible in another functional component with the help of props drill or by context api. But, the best way is the Redux store now a days. Where, we pass the openCompose and setOpenCompose state there and can be access any where in the program. So, install react-redux and @reduxjs/toolkit.
+
+    const openCompose = useSelector((store) => store.appy.openCompose);
+
     return (
-        <div className="bg-white ma-w-dxl shadow-xl shadow-slate-600 rounded-t-md">
+        <div className={`${openCompose ? "block" : "hidden"} bg-white ma-w-dxl shadow-xl shadow-slate-600 rounded-t-md`}>
             {/* Starting of Compose BODY Section (First-Row) */}
             <div className="flex px-3 py-2 bg-[#F2F6Fc] justify-between rounded-t-md">
                 {/* Starting of Compose BODY Right Section (First-Row) */}
