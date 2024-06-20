eb5666e42aff59888952dae6c21f0a1f741e5dd8
CoolShivas <shivas2710cool00@gmail.com>
2024-06-20 13:06:30 +0530

    Formation of INITIAL_VALUE  by using createSlice and declare the reducers state and action.

--- mail_box_clone/src/components/store/reduxStore.js
+++ mail_box_clone/src/components/store/reduxStore.js
@@ -1,9 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+const INITIAL_VALUE = {
+  openCompose: false,
+};
+
+const reduxSlice = createSlice({
+  name: "mail_box",
+  initialState: { INITIAL_VALUE },
+  reducers: {
+    setOpenCompose: (state, action) => {
+      state.openCompose = action.payload;
+    },
+  },
+});
+
 const reduxStore = configureStore({
   reducer: {
     appy: reduxSlice.reducer,
   },
 });
+
+export const { setOpenCompose } = reduxSlice.actions;
 
 export default reduxStore;
